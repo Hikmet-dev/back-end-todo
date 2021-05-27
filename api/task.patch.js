8c1656df04db9c20a88559c82a973a12505d3d69
Hikmet Mehdi-zade <hikmetmeh@gmail.com>
2021-05-27 17:15:05 +0300

    Api Patch func

--- api/task.patch.js
+++ api/task.patch.js
@@ -1,6 +1,33 @@
+import fs from 'fs';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 
 export const PATCH = (req, res) => {
         let idParam = req.params['idParam'];
-        res.send('Patch request task:  ' + idParam);
+        let body = req.body;
+
+        fs.readFile(__dirname + '/tasks.json', 'utf-8', (err, data) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(typeof idParam);
+        
+                const taskList = JSON.parse(data.toString());
+                console.log(taskList);
+                const index = taskList.findIndex(item => item.id === idParam);
+                console.log(index);
+                const newObjs = {...taskList[index], ...body };
+                taskList[index] = newObjs;
+
+                
+
+                fs.writeFile(__dirname + '/tasks.json', JSON.stringify(taskList, null, 2), (err) => {
+                    if(err)  {
+                        console.log(err);
+                    }
+                    res.send(`patch: ${idParam}`);
+                });
+            });
 };
